cf1bb5ffb2d0b27acd0eb28a9fa3f618834c08c6
Nalin Dahyabhai <nalin@redhat.com>
2004-04-21 15:42:32 +0000

    * configure.ac: search for PAM libraries separately * src/Makefile.am: use a convenience library to compile code only once * src/afs5log.c: supply a non-bogus ccache and options argument to minikafs, 	provide local logging functions which use stdio.

--- src/afs5log.c
+++ src/afs5log.c
@@ -34,15 +34,78 @@
 
 #include <sys/types.h>
 #include <limits.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 #include <krb5.h>
+#ifdef USE_KRB4
+#include KRB4_DES_H
+#include KRB4_KRB_H
+#ifdef KRB4_KRB_ERR_H
+#include KRB4_KRB_ERR_H
+#endif
+#endif
+#include <security/pam_appl.h>
 
+#include "log.h"
+#include "options.h"
+#include "stash.h"
 #include "minikafs.h"
-#include "minikafs.c"
+
+static struct _pam_krb5_options options;
+
+void
+debug(const char *fmt, ...)
+{
+	va_list va;
+	char *fmt2;
+	fmt2 = malloc(strlen(fmt) + strlen("afs5log: \n") + 1);
+	if (fmt2 == NULL) {
+		return;
+	}
+	sprintf(fmt2, "afs5log: %s\n", fmt);
+	if (options.debug > 0) {
+		va_start(va, fmt);
+		vfprintf(stderr, fmt2, va);
+		va_end(va);
+	}
+	free(fmt2);
+}
+
+void
+notice(const char *fmt, ...)
+{
+	va_list va;
+	char *fmt2;
+	fmt2 = malloc(strlen(fmt) + strlen("afs5log: \n") + 1);
+	if (fmt2 == NULL) {
+		return;
+	}
+	sprintf(fmt2, "afs5log: %s\n", fmt);
+	va_start(va, fmt);
+	vfprintf(stderr, fmt2, va);
+	va_end(va);
+	free(fmt2);
+}
+
+void
+crit(const char *fmt, ...)
+{
+	va_list va;
+	char *fmt2;
+	fmt2 = malloc(strlen(fmt) + strlen("afs5log: \n") + 1);
+	if (fmt2 == NULL) {
+		return;
+	}
+	sprintf(fmt2, "afs5log: %s\n", fmt);
+	va_start(va, fmt);
+	vfprintf(stderr, fmt2, va);
+	va_end(va);
+	free(fmt2);
+}
 
 int
 main(int argc, char **argv)
@@ -50,26 +113,46 @@ main(int argc, char **argv)
 	char local[PATH_MAX], home[PATH_MAX];
 	const char *homedir;
 	int i, j, try_v5_2b, cells;
+	krb5_context ctx;
+	krb5_ccache ccache;
 	uid_t uid;
 
 	/* Iterate through every parameter, assuming they're names of cells. */
 	try_v5_2b = 0;
 	cells = 0;
 	uid = getuid();
+	memset(&options, 0, sizeof(options));
+	memset(&ccache, 0, sizeof(ccache));
+	i = krb5_init_context(&ctx);
+	if (i != 0) {
+		fprintf(stderr, "Error initializing Kerberos: %d\n", i);
+		exit(1);
+	}
+	i = krb5_cc_default(ctx, &ccache);
+	if (i != 0) {
+		fprintf(stderr, "Error reading default credential cache: %d\n",
+			i);
+		exit(1);
+	}
 	for (i = 1; i < argc; i++) {
 		if (argv[i][0] == '-') {
 			switch (argv[i][1]) {
 			case '5':
 				try_v5_2b = !try_v5_2b;
 				break;
+			case 'v':
+				options.debug++;
+				break;
 			default:
-				printf("%s: [ [-5] [cell] ] [...]\n", argv[0]);
+				printf("%s: [ [-v] [-5] [cell] ] [...]\n",
+				       argv[0]);
+				krb5_free_context(ctx);
 				exit(0);
 				break;
 			}
 		} else {
 			cells++;
-			j = minikafs_log(NULL, NULL,
+			j = minikafs_log(NULL, ccache, &options,
 					 argv[i], uid, try_v5_2b);
 			if (j != 0) {
 				fprintf(stderr, "%s: %d\n", argv[i], j);
@@ -82,7 +165,7 @@ main(int argc, char **argv)
 	if (cells == 0) {
 		j = minikafs_cell_of_file("/afs", local, sizeof(local));
 		if (j == 0) {
-			j = minikafs_log(NULL, NULL,
+			j = minikafs_log(NULL, ccache, &options,
 					 local, uid, try_v5_2b);
 			if (j != 0) {
 				fprintf(stderr, "%s: %d\n", local, j);
@@ -94,7 +177,7 @@ main(int argc, char **argv)
 				j = minikafs_cell_of_file(homedir,
 							  home, sizeof(home));
 				if ((j == 0) && (strcmp(local, home) != 0)) {
-					j = minikafs_log(NULL, NULL,
+					j = minikafs_log(NULL, ccache, &options,
 							 home, uid, try_v5_2b);
 					if (j != 0) {
 						fprintf(stderr, "%s: %d\n",
@@ -104,5 +187,6 @@ main(int argc, char **argv)
 			}
 		}
 	}
+	krb5_free_context(ctx);
 	return 0;
 }
