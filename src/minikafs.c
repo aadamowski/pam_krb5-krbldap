e041a903db4a45aa15b18ab81d256c53516d0b38
Nalin Dahyabhai <nalin@redhat.com>
2004-09-02 19:47:28 +0000

    - Ioctl patch via bugzilla looked a bit too much like heimdal.  Add copyright   statement from heimdal, with secondhand okay to use the same license for that   code as the rest of the module.  Suppress urge to just rewrite the code in   question, at least for now.

--- src/minikafs.c
+++ src/minikafs.c
@@ -1,5 +1,6 @@
 /*
  * Copyright 2004 Red Hat, Inc.
+ * Copyright 2004 Kungliga Tekniska Högskolan
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
