6cefc9d425ad3fa95f4b4f8aedc7198beb436f5f
Nalin Dahyabhai <nalin.dahyabhai@pobox.com>
2009-05-27 18:49:52 -0400

    - update copyright dates

--- src/options.c
+++ src/options.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003,2004,2005,2006,2007,2008 Red Hat, Inc.
+ * Copyright 2003,2004,2005,2006,2007,2008,2009 Red Hat, Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
