5108fa4a7912374635a64046b19c568ccb8213c9
Nalin Dahyabhai <nalin@dahyabhai.net>
2011-01-31 17:45:40 -0500

    expect the right warning at the right place, and the right acct-mgmt result

--- tests/005-pamchpw-success/run.sh
+++ tests/005-pamchpw-success/run.sh
@@ -5,4 +5,7 @@ source $testdir/testenv.sh
 echo "";echo Succeed: correct password, expired, change.
 $kadmin -q 'cpw -pw foo '$test_principal 2> /dev/null > /dev/null
 $kadmin -q 'modprinc -pwexpire now '$test_principal 2> /dev/null > /dev/null
-test_run -auth -account -chauthtok -setcred -session $test_principal $pam_krb5 $test_flags no_warn -- foo foo bar bar baz baz
+test_kdcstop
+test_kdcstart
+test_settle
+test_run -auth -account -chauthtok -setcred -session $test_principal $pam_krb5 $test_flags -- foo foo bar bar baz baz
