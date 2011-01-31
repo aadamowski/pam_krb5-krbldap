2b2495e05f7e2452b15ce4464d56244340a051ff
Nalin Dahyabhai <nalin@dahyabhai.net>
2011-01-31 18:21:56 -0500

    - request a similarly-shortened regular lifetime

--- tests/013-options-renew/run.sh
+++ tests/013-options-renew/run.sh
@@ -7,4 +7,4 @@ $kadmin -q 'cpw -pw foo '$test_principal 2> /dev/null > /dev/null
 $kadmin -q 'modprinc -pwexpire never '$test_principal 2> /dev/null > /dev/null
 
 echo "";echo Renewable lifetime 1 hour.
-test_run -auth -setcred $test_principal -run klist_t $pam_krb5 $test_flags proxiable forwardable not_addressless renew_lifetime=3600 -- foo
+test_run -auth -setcred $test_principal -run klist_t $pam_krb5 $test_flags proxiable forwardable not_addressless ticket_lifetime=3600 renew_lifetime=3600 -- foo
