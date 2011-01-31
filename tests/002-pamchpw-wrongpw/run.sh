7975006ddf6105054ee5be8e4f9e0a94865d95dc
Nalin Dahyabhai <nalin@dahyabhai.net>
2011-01-31 14:30:41 -0500

    - don't do some work we don't need to any more

--- tests/002-pamchpw-wrongpw/run.sh
+++ tests/002-pamchpw-wrongpw/run.sh
@@ -2,10 +2,6 @@
 
 source $testdir/testenv.sh
 
-echo "";echo Expiring password.
-$kadmin -q 'cpw -pw foo '$test_principal 2> /dev/null > /dev/null
-$kadmin -q 'modprinc -pwexpire now '$test_principal 2> /dev/null > /dev/null
-
 echo "";echo Fail: incorrect password.
 $kadmin -q 'cpw -pw foo '$test_principal 2> /dev/null > /dev/null
 $kadmin -q 'modprinc -pwexpire now '$test_principal 2> /dev/null > /dev/null
