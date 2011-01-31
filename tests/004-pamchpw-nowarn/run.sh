24380d1d77f51a9b09ccb0cd75d137e70e61961c
Nalin Dahyabhai <nalin@dahyabhai.net>
2011-01-31 14:28:19 -0500

    - rename, this is the nowarn case

---
+++ tests/004-pamchpw-nowarn/run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+source $testdir/testenv.sh
+
+echo "";echo Succeed: correct password, do not warn about expiration.
+$kadmin -q 'cpw -pw foo '$test_principal 2> /dev/null > /dev/null
+$kadmin -q 'modprinc -pwexpire now '$test_principal 2> /dev/null > /dev/null
+test_run -auth -account $test_principal $pam_krb5 $test_flags no_warn -- foo
