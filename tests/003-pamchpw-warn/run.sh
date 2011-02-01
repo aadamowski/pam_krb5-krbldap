7793dde2042c232435f8e3f05f36e192a52c06c2
Nalin Dahyabhai <nalin@dahyabhai.net>
2011-02-01 10:49:32 -0500

    - we don't need to restart here, we just need to wait

--- tests/003-pamchpw-warn/run.sh
+++ tests/003-pamchpw-warn/run.sh
@@ -5,7 +5,5 @@ source $testdir/testenv.sh
 echo "";echo Succeed: correct password, warn about expiration.
 $kadmin -q 'cpw -pw foo '$test_principal 2> /dev/null > /dev/null
 $kadmin -q 'modprinc -pwexpire now '$test_principal 2> /dev/null > /dev/null
-test_kdcstop
-test_kdcstart
 test_settle
 test_run -auth -account $test_principal $pam_krb5 $test_flags -- foo bar bar
