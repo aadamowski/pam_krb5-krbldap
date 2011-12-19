585187d4a663367ca2b4f857806a28a025831a18
Aleksander Adamowski <aleksander.adamowski@gmail.com>
2011-12-19 07:15:02 +0100

    Use LD_LIBRARY_PATH in krbldap test script to link to customized libkrk5

--- tests/run-tests-krbldap-direct.sh
+++ tests/run-tests-krbldap-direct.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+export LD_LIBRARY_PATH=/var/soft/PAM/usr/lib
 export KRB5_TRACE=/dev/stdout
 TESTROOTDIR=$(dirname $0)
 $TESTROOTDIR/tools/pam_harness -auth alice $TESTROOTDIR/../src/.libs/pam_krbldap.so  ignore_afs -- bar 2>&1
