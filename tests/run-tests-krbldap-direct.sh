7b402a84d103415c0ec7a1b5a9b6c73f3d5ddd6b
Aleksander Adamowski <aleksander.adamowski@gmail.com>
2011-12-19 07:46:26 +0100

    Revert to vanilla pam_krb5 for tests after migrating KrbLDAP logic to customized libkrb5.

--- tests/run-tests-krbldap-direct.sh
+++ tests/run-tests-krbldap-direct.sh
@@ -3,4 +3,4 @@
 export LD_LIBRARY_PATH=/var/soft/PAM/usr/lib
 export KRB5_TRACE=/dev/stdout
 TESTROOTDIR=$(dirname $0)
-$TESTROOTDIR/tools/pam_harness -auth alice $TESTROOTDIR/../src/.libs/pam_krbldap.so  ignore_afs -- bar 2>&1
+$TESTROOTDIR/tools/pam_harness -auth alice $TESTROOTDIR/../src/.libs/pam_krb5.so  ignore_afs -- bar 2>&1
